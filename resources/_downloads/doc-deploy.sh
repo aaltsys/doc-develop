a20357d570bdd31afa7fd545c9fb4aaa211d1be7
Gerald Lovel <gerald@lovels.us>
2013-10-26 09:50:39 -0500

    Deployed documentation

--- resources/_downloads/doc-deploy.sh
+++ resources/_downloads/doc-deploy.sh
@@ -222,8 +222,8 @@ else
           if [[ $RC > 0 ]] ; then echo "$(pwd)$(tput setaf 1) $LINENO: cp -RH $DIR_OUT/.ht* ../$DIR_DEPLOY/ $(tput sgr0)" ; fi
           # If it exists, delete CNAME from master deploy subdirectory
           if [[ -e $DIR_OUT/CNAME ]] ; then
-            echo "CNAME $(<../$DIR_DEPLOY/$MASTER/CNAME) found in ../$DIR_DEPLOY/$MASTER/CNAME"
-            rm $DIR_DEPLOY/$MASTER/CNAME
+            echo "CNAME $(<../$DIR_OUT/CNAME) found in ../$DIR_OUT/CNAME"
+            rm $DIR_OUT/CNAME
           fi
         fi
       cd ..
