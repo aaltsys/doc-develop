a9ed4724a04391ea3f22d65b5ef91d2c7c2bfcda
Gerald Lovel <glovel@aaltsys.com>
2014-04-19 08:43:38 -0500

    Deployed documentation

--- python/searchindex.js
+++ python/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["AAltsys <strong>Python Language</strong> Tutorial"],objects:{},titleterms:{aaltsi:0,indic:0,python:0,tutori:0,tabl:0,strong:0,languag:0}})
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["AAltsys <strong>Python Language</strong> Tutorial"],objects:{},titleterms:{aaltsi:0,indic:0,python:0,tutori:0,tabl:0,languag:0}})
